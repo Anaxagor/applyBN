4db739de26551c583218270f49ce355d9e425a95
ilia269 <ilya.kuptcov@yandex.ru>
2024-10-05 02:44:36 +0300

    balance function fixed

--- applybn/data_generation/class_balance.py
+++ applybn/data_generation/class_balance.py
@@ -1,34 +1,32 @@
 from applybn.data_generation.base_class import DataGenerator
-from bamt.networks.base import BaseNetwork
 from pandas import DataFrame, concat
 from typing import Union
 from applybn.core import copy_data
 
 
-class BNClassBalance(DataGenerator):
+class BNClassBalancer(DataGenerator):
     """
-    Class to balance unbalanced data.
+    Class to balance disbalanced data.
 
     Methods:
         balance:
     """
     def __init__(
             self,
-            bn: BaseNetwork
     ):
-        super().__init__(bn)
+        super().__init__()
 
-    @copy_data
+    # @copy_data
     def balance(self,
-                unbalanced_data: DataFrame,
-                class_column: Union[str, None],
-                strategy: Union[str, int],
-                shuffle: bool,
+                disbalanced_data: DataFrame,
+                class_column: Union[str, None] = None,
+                strategy: Union[str, int] = 'max_class',
+                shuffle: bool = True,
                 ):
         """
 
         Args:
-            unbalanced_data: data with unbalanced classes
+            disbalanced_data: data with disbalanced classes
             class_column: name of class column in data
             strategy: whether all class sizes are equal to currently maximum class (str "max_class") size
             or manually chosen size (int n)
@@ -38,8 +36,8 @@ class BNClassBalance(DataGenerator):
             balanced data
 
         """
-        class_column = class_column if class_column is not None else unbalanced_data.columns[-1]
-        classes_size = unbalanced_data.value_counts(unbalanced_data[class_column], sort=True)
+        class_column = class_column if class_column is not None else disbalanced_data.columns[-1]
+        classes_size = disbalanced_data.value_counts(disbalanced_data[class_column], sort=True)
 
         if strategy == 'max_class':
             max_class_size = classes_size[0]
@@ -47,16 +45,16 @@ class BNClassBalance(DataGenerator):
             max_class_size = strategy
 
         additional_data_lst = []
-        for cur_class in unbalanced_data[class_column].unique():
+        for cur_class in disbalanced_data[class_column].unique():
             cur_class_size = classes_size[cur_class]
             need_size = 0 if max_class_size - cur_class_size < 0 else max_class_size - cur_class_size
 
-            additional_data_lst.append(self.bn.sample(evidence={class_column: cur_class},
-                                                      n=need_size))
+            if need_size:
+                additional_data_lst.append(self.bn.sample(need_size, evidence={'label': 0})[disbalanced_data.columns])
 
-        balanced_data = concat([unbalanced_data] + additional_data_lst)
+        balanced_data = concat([disbalanced_data] + additional_data_lst)
 
         if shuffle:
             balanced_data = balanced_data.sample(frac=1).reset_index(drop=True)
 
-        return balanced_data
+        return balanced_data[disbalanced_data.columns].astype(disbalanced_data.dtypes.to_dict())
